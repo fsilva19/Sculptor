fd1a5d41afaec617f668c44a01be4859697c15ac
N1LB13 <107091708+N1LB13@users.noreply.github.com>
2023-05-28 19:03:17 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,25 +15,28 @@ int main()
     sculptor.setColor(1.0, 0.0, 0.0, 1.0); // Vermelho opaco
 
     // Desenho do cubo
-    sculptor.putBox(0, 4, 0, 4, 0, 4);
+    sculptor.putBox(2, 7, 2, 7, 2, 7);
 
     // Removendo parte interna do cubo
-    sculptor.cutBox(1,3,1,3,1,3);
+    sculptor.cutBox(3,6,3,6,3,6);
 
     // Definição das cores
     sculptor.setColor(0.0, 1.0, 0.0, 1.0); // Verde opaco
 
     // Desenho da esfera
-    sculptor.putSphere(10, 5, 5, 3);
+    sculptor.putSphere(16, 5, 5, 3);
 
     // Removendo parte interna da esfera
-    sculptor.cutSphere(10, 5, 5, 2);
+    sculptor.cutSphere(16, 5, 5, 2);
 
     // Definição das cores
     sculptor.setColor(0.0, 0.0, 1.0, 1.0); // Azul opaco
 
     // Desenho do elipsoide
-    sculptor.putEllipsoid(3, 9, 5, 3, 2, 2);
+    sculptor.putEllipsoid(10, 12, 5, 4, 3, 3);
+
+    // Removendo parte interna do elipsoide
+    sculptor.cutEllipsoid(10, 12, 5, 3, 2, 2);
 
     // Gravação do Sculptor em formato OFF
     sculptor.writeOFF("exemplo.off");
